91555fe83c7a61ede675648275f62c4b501dd22f
jdharshini670-spec <jdharshini670@gmail.com>
2026-09-02 20:33:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+function myFunction() {
+    alert("Welcome to PLANTORA!");
+}
+
+function flower() {
+    location.href = "hibicus-flower.html";
+}
+
+function plant() {
+    location.href = "money-plant.html";
+}
+
+function aleon() {
+    location.href = "aleon.html";
+}
+
+function roseplant() {
+    location.href = "roseplant.html";
+}
+
+function sangupoo() {
+    location.href = "sangupoo.html";
+}
+
+function tablerosePlant() {
+    location.href = "table-rose.html";
+}
+
+function bambooshoots() {
+    location.href = "bambooshoots.html";
+}
+
+function productBuy() {
+    location.href = "payment.html";
+}
+
+function buynow() {
+    location.href = "payment.html";
+}
+
+function Login() {
+    location.href = "login.html";
+}
